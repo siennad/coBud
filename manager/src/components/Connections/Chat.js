ac163240416530775ab893bc3b5f514785ab9d50
siennad <siennad97@gmail.com>
2019-02-21 22:59:46 +0200

    merge

---
+++ manager/src/components/Connections/Chat.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Container, Content, Header, Item, Icon, Input, Text } from 'native-base';
+import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux';
+import MainFooterBar from '../common/MainFooterBar';
+import { navigateToConnections } from '../../actions/NavigationActions';
+
+class Chat extends Component {
+
+  componentDidMount() {
+    this.props.navigateToConnections();
+  }
+
+  componentWillUnmount() {
+    Actions.pop();
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon ios="ios-search" android="md-search" />
+            <Input placeholder="Search..." />
+          </Item>
+        </Header>
+        <Content>
+          <Item>
+            <Text>connections</Text>
+          </Item>
+        </Content>
+        <MainFooterBar page={this.props.sceneKey} />
+      </Container>
+    );
+  }
+}
+
+export default connect(undefined, { navigateToConnections })(Chat);
